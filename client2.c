341f84a605abba747c67ea839c6ec34153f48563
Abhishek Sharma <54661594+abhisheksharma1395@users.noreply.github.com>
2022-10-05 19:18:33 -0700

    client2 modified

--- client2.c
+++ client2.c
@@ -14,6 +14,12 @@ int main(int argc, char *argv[])
   int sockfd = 0,n = 0;
   char recvBuff[1024];
   struct sockaddr_in serv_addr;
+
+  char hostname[1024];
+  strcpy(hostname, "server.ashar.cs164");
+  struct  hostent  *ptrh;
+  ptrh = gethostbyname(hostname);
+
  
   memset(recvBuff, '0' ,sizeof(recvBuff));
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
@@ -21,10 +27,16 @@ int main(int argc, char *argv[])
       printf("\n Error : Could not create socket \n");
       return 1;
     }
+
+  
+
+  if( argc == 2 ) {
+      printf("Client says, %s\n", argv[1]);
+  }
  
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(5000);
-  serv_addr.sin_addr.s_addr = inet_addr("server.ashar.cs164");
+  serv_addr.sin_addr.s_addr = inet_addr(ptrh->h_addr);
  
   if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
     {
@@ -32,9 +44,7 @@ int main(int argc, char *argv[])
       return 1;
     }
 	
-	if( argc == 2 ) {
-      printf("Client says, %s\n", argv[1]);
-  }
+	
 	
   while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
     {
